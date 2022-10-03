06098a98edf92ddb845600bbc0376a5451472ab9
inucat <70513648+inucat@users.noreply.github.com>
2022-10-03 15:26:32 +0900

    Prettified & comment modify

--- src/Main.c
+++ src/Main.c
@@ -1,4 +1,4 @@
-#define UNICODE                             // UTF-8 support
+#define UNICODE ///< UTF-8 support
 #include <windows.h>
 #include "Resource.h"
 #include "Procedures.h"
@@ -6,35 +6,37 @@
 /// Entry point
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE _$, LPSTR lpszCmdLine, int nCmdShow)
 {
-    TCHAR * const wndclass_name = TEXT(STR_CODENAME);
+    TCHAR *const wndclass_name = TEXT(STR_CODENAME);
     WNDCLASS wc;
     HWND hwnd;
     MSG msg;
 
     // Set Window Attributes & register Window class with them
-    wc.style            = CS_HREDRAW|CS_VREDRAW;
-    wc.lpfnWndProc      = WndProc;
-    wc.cbClsExtra       = 0;
-    wc.cbWndExtra       = 0;
-    wc.hInstance        = hInstance;
-    wc.hIcon            = NULL;
-    wc.hCursor          = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground    = (HBRUSH) (COLOR_ACTIVECAPTION + 1);
-    wc.lpszMenuName     = NULL;
-    wc.lpszClassName    = wndclass_name;
-    if ( !RegisterClass(&wc) ) return 0;
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = NULL;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_ACTIVECAPTION + 1);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = wndclass_name;
+    if (!RegisterClass(&wc))
+        return 0;
 
     hwnd = CreateWindow(
         wndclass_name,
         TEXT(STR_APPNAME),
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-        NULL, NULL, hInstance, NULL
-    );
-    if ( !hwnd ) return 0;
+        NULL, NULL, hInstance, NULL);
+    if (!hwnd)
+        return 0;
 
     // Message loop, repeats until app terminates
-    while ( GetMessage(&msg, NULL, 0, 0) > 0 ) {
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
@@ -42,53 +44,46 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE _$, LPSTR lpszCmdLine, int nCm
     return msg.wParam;
 }
 
-// Window Procedure
-LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    #define Call_WMHandler(handler_name) handler_name(hwnd, uMsg, wParam, lParam)
-    switch (uMsg) {
-    // when apps started
-    case WM_CREATE:
+/// Window Procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+#define Call_WMHandler(handler_name) handler_name(hwnd, uMsg, wParam, lParam)
+    switch (uMsg)
+    {
+    case WM_CREATE: // when apps started
         Call_WMHandler(wmCreate);
         return 0;
 
-    // when icon is clicked
-    case AWM_TRAYICONCLICKED:
+    case AWM_TRAYICONCLICKED: // when icon is clicked
         Call_WMHandler(wmTrayIconClicked);
         return 0;
 
-    // when menu item is clicked
-    case WM_COMMAND:
+    case WM_COMMAND: // when menu item is clicked
         Call_WMHandler(wmMenuItemClicked);
         return 0;
 
-    // when toggle key is pressed
-    case AWM_LLKEYHOOKED: {
+    case AWM_LLKEYHOOKED: // when toggle key is pressed
         Call_WMHandler(wmLLKeyHooked);
         return 0;
-    }
 
-    // when theme mode has changed
-    case WM_SETTINGCHANGE:
+    case WM_SETTINGCHANGE: // when theme mode has changed
         Call_WMHandler(wmThemeChanged);
         return 0;
 
-    // when leaves the session
-    case WM_QUERYENDSESSION:
+    case WM_QUERYENDSESSION: // when leaves the session
         Call_WMHandler(wmOnExit);
         return TRUE;
 
-    // when closes window
-    case WM_CLOSE:
+    case WM_CLOSE: // when closes window
         Call_WMHandler(wmOnExit);
         DestroyWindow(hwnd);
         return 0;
 
-    // when destroy window
-    case WM_DESTROY:
+    case WM_DESTROY: // when destroy window
         Call_WMHandler(wmDestroy);
         return 0;
     } // switch (uMsg)
 
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
-    #undef Call_WMHandler
+#undef Call_WMHandler
 }
